5ec9b6a5275ba3eb6fd1d06cf3eaed1b868ad4f6
Sudarshan Srinivasan <sudarshan85@gmail.com>
2018-01-31 15:32:38 -0500

    Simplified code to just read directly from relevant cols file

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # NLPSolve
 
 This project contains code from the tutorial [How to solve 90% of NLP problems: a step-by-step guide](https://blog.insightdatascience.com/how-to-solve-90-of-nlp-problems-a-step-by-step-guide-fda605278e4e).
-The data for the project can be downloaded [here](https://www.crowdflower.com/wp-content/uploads/2016/03/socialmedia-disaster-tweets-DFE.csv).
+The original data for the project can be downloaded [here](https://www.crowdflower.com/wp-content/uploads/2016/03/socialmedia-disaster-tweets-DFE.csv). This data is not formated
+correctly and has line folding. The formated data for the project with only relevant columns can be found on their repo and downloaded [here](https://raw.githubusercontent.com/hundredblocks/concrete_NLP_tutorial/master/socialmedia_relevant_cols.csv).
 
 ###### Note: The CSV file in the link is encoded with ISO-8859-1 encoding. So all reading methods should account for that.
--- clean_data.py
+++ clean_data.py
@@ -4,39 +4,18 @@ import pandas as pd
 import codecs
 from io import StringIO
 
-def extract_data(data):
-    """
-    Extract the relevant columns from the raw dataframe
-    :param data: dataframe from which to extract columns
-    :return: extracted dataframe
-    """
-    x = data.loc[:, ['text', 'choose_one', 'choose_one:confidence']]
-    x.rename(columns={'choose_one:confidence': 'class_label'}, inplace=True)
-    x.loc[x.choose_one == 'Relevant', 'class_label'] = 1
-    x.loc[x.choose_one == 'Not Relevant', 'class_label'] = 0
+def get_df(fname):
+    # Read file into a string buffer with codecs for converting encoding
+    with codecs.open(fname, 'r', encoding='utf-8', errors='replace') as inf:
+        csv_strio = StringIO(inf.read())
 
-    return x
-
-def get_csv():
-    # Read file into a string with codecs for converting encoding
-    csv_str = ""
-    with codecs.open('disaster-tweets.csv', 'r', encoding='utf-8', errors='replace') as inf:
-        for line in inf:
-            csv_str += line
-
-    # convert raw string int string buffer for steaming
-    csv_str = StringIO(csv_str)
-
-    return csv_str
+    df = pd.read_csv(csv_strio)
+    return df
 
 def main():
-    csv_file = get_csv()
-    full_data = pd.read_csv(csv_file)
-    qm = extract_data(full_data)
-    print(qm.head)
-    print(qm.shape)
-
-    # extracted_data.to_pickle('cleaned-df.pkl')
+    questions = get_df('socialmedia_relevant_cols.csv')
+    print(questions.head())
+    print(questions.shape)
 
 if __name__=='__main__':
     main()
