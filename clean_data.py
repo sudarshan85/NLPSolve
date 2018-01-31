3caab3c379c4df167bf17c62484ce133763e2a91
Sudarshan Srinivasan <sudarshan85@gmail.com>
2018-01-31 14:09:34 -0500

    Renamed get_relevant to clean_data

--- clean_data.py
+++ clean_data.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python
+"""
+Script to extract the relevant columns from the raw CSV using Pandas and storing
+it in a pickle file
+"""
 
 import pandas as pd
 
 def extract_data(data):
+    """
+    Extract the relevant columns from the raw dataframe
+    :param data: dataframe from which to extract columns
+    :return: extracted dataframe
+    """
     x = data.loc[:, ['text', 'choose_one', 'choose_one:confidence']]
     x.rename(columns={'choose_one:confidence': 'class_label'}, inplace=True)
     x.loc[x.choose_one == 'Relevant', 'class_label'] = 1
@@ -12,8 +21,8 @@ def extract_data(data):
 
 def main():
     full_data = pd.read_csv('disaster-tweets.csv', encoding='ISO-8859-1')
-    extracted_data = extract_data(full_data)
-    extracted_data.to_pickle('cleaned-df.pkl')
+    questions = extract_data(full_data)
+    # extracted_data.to_pickle('cleaned-df.pkl')
 
 if __name__=='__main__':
     main()
