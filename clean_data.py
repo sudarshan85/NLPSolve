f63266edaf3bfa105727ff2b9c885cbefb712301
Sudarshan Srinivasan <sudarshan85@gmail.com>
2018-01-31 13:01:41 -0500

    Basic pandas dataframe refactorization
    Created new pandas df from *original* dataset with refactored data
    as given in the csv used in the tutorial

--- clean_data.py
+++ clean_data.py
@@ -2,18 +2,18 @@
 
 import pandas as pd
 
+def extract_data(data):
+    x = data.loc[:, ['text', 'choose_one', 'choose_one:confidence']]
+    x.rename(columns={'choose_one:confidence': 'class_label'}, inplace=True)
+    x.loc[x.choose_one == 'Relevant', 'class_label'] = 1
+    x.loc[x.choose_one == 'Not Relevant', 'class_label'] = 0
+
+    return x
+
 def main():
-    # tweets = []
-    # with open('disaster_tweets.csv', 'r', encoding='ISO-8859-1') as csvfile:
-    #     reader = csv.reader(csvfile)
-    #     for row in reader:
-    #         tweets.append(row)
-    #
-    # print(len(tweets))
-
-    tweets = pd.read_csv('disaster_tweets.csv', encoding='ISO-8859-1')
-    print(tweets.head())
-
+    full_data = pd.read_csv('disaster_tweets.csv', encoding='ISO-8859-1')
+    extracted_data = extract_data(full_data)
+    extracted_data.to_pickle('cleaned_df.pkl')
 
 if __name__=='__main__':
     main()
