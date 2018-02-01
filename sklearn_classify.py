588295992a850068afc499710c057ea462a2b6a3
Sudarshan Srinivasan <sudarshan85@gmail.com>
2018-02-01 16:01:29 -0500

    Moved all plot functions to separate file

--- sklearn_classify.py
+++ sklearn_classify.py
@@ -1,19 +1,13 @@
 #!/usr/bin/env python
 
 import pandas as pd
-import numpy as np
-import itertools
-
-
-import matplotlib.pyplot as plt
-import matplotlib
-import matplotlib.patches as mpatches
 
 from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from sklearn.decomposition import PCA, TruncatedSVD
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, classification_report, confusion_matrix
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, confusion_matrix
+
+from plot_functions import *
 
 def cv(data):
     count_vec = CountVectorizer()
@@ -21,46 +15,8 @@ def cv(data):
 
     return emb, count_vec
 
-def plot_LSA(test_data, test_labels, plot=True):
-    lsa = TruncatedSVD(n_components=2)
-    lsa.fit(test_data)
-    lsa_scores = lsa.transform(test_data)
-    color_mapper = {label: idx for idx, label in enumerate(set(test_labels))}
-    color_column = [color_mapper[label] for label in test_labels]
-    colors = ['orange', 'blue', 'blue']
-    if plot:
-        plt.scatter(lsa_scores[:, 0], lsa_scores[:, 1], s=8, alpha=.8, c=test_labels,
-                    cmap=matplotlib.colors.ListedColormap(colors))
-        red_patch = mpatches.Patch(color='orange', label='Irrelevant')
-        green_patch = mpatches.Patch(color='blue', label='Disaster')
-        plt.legend(handles=[red_patch, green_patch], prop={'size': 30})
 
 
-def plot_confusion_matrix(cm, classes,
-                          normalize=False,
-                          title='Confusion matrix',
-                          cmap=plt.cm.winter):
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title, fontsize=30)
-    plt.colorbar()
-    tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, fontsize=20)
-    plt.yticks(tick_marks, classes, fontsize=20)
-
-    fmt = '.2f' if normalize else 'd'
-    thresh = cm.max() / 2.
-
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, format(cm[i, j], fmt), horizontalalignment="center",
-                 color="white" if cm[i, j] < thresh else "black", fontsize=40)
-
-    plt.tight_layout()
-    plt.ylabel('True label', fontsize=30)
-    plt.xlabel('Predicted label', fontsize=30)
-
-    # return plt
 
 def get_metrics(y_test, y_predicted):
     # true positives / (true positives+false positives)
@@ -78,6 +34,23 @@ def get_metrics(y_test, y_predicted):
 
     return accuracy, precision, recall, f1
 
+
+def get_most_important_features(vectorizer, model, n=5):
+    index_to_word = {v: k for k, v in vectorizer.vocabulary_.items()}
+
+    # loop for each class
+    classes = {}
+    for class_index in range(model.coef_.shape[0]):
+        word_importances = [(el, index_to_word[i]) for i, el in enumerate(model.coef_[class_index])]
+        sorted_coeff = sorted(word_importances, key=lambda x: x[0], reverse=True)
+        tops = sorted(sorted_coeff[:n], key=lambda x: x[0])
+        bottom = sorted_coeff[-n:]
+        classes[class_index] = {
+            'tops': tops,
+            'bottom': bottom
+        }
+    return classes
+
 def main():
     questions = pd.read_pickle('ready_data.pkl')
 
@@ -86,7 +59,7 @@ def main():
     X_train, X_test, y_train, y_test = train_test_split(list_corpus, list_labels, test_size=0.2, random_state=40)
     X_train_counts, count_vec = cv(X_train)
     X_test_counts = count_vec.transform(X_test)
-    # plt.figure(figsize=(16, 16))
+
     # plot_LSA(X_train_counts, y_train)
 
     clf = LogisticRegression(C=30.0, class_weight='balanced', solver='newton-cg',
@@ -96,10 +69,8 @@ def main():
 
     accuracy, precision, recall, f1 = get_metrics(y_test, y_predicted_counts)
     cm = confusion_matrix(y_test, y_predicted_counts)
-    plt.figure(figsize=(10, 10))
-    plot_confusion_matrix(cm, classes=['Irrelevant', 'Disaster', 'Unsure'], normalize=False,
-                                 title='Confusion matrix')
-    plt.show()
+
+    plot_confusion_matrix(cm, classes=['Irrelevant', 'Disaster', 'Unsure'], normalize=False, title='Confusion matrix')
     print(cm)
     print("accuracy = %.3f, precision = %.3f, recall = %.3f, f1 = %.3f" % (accuracy, precision, recall, f1))
 
